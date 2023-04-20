21c86bec166dffa363504904ed2244bb9a56eeb8
emiliamassing <emiliamassings@gmail.com>
2023-04-20 12:43:23 +0200

    💄Added CSS to infoContainer, ✨Created timer function

--- frontend/scripts/gameplay.js
+++ frontend/scripts/gameplay.js
@@ -1,3 +1,40 @@
+import { renderGridContainer } from "./gridDrawing";
+import { renderUserChat } from "./userChat";
+
+const app = document.querySelector('#app');
+
 export function startGame() {
     console.log('Spelet är igång');
+
+    app.innerHTML = '';
+    renderUserChat();
+    printPreviewCountdown();
+    renderGridContainer();
 };
+
+function printPreviewCountdown() {
+    console.log('Count down');
+    
+    const container = document.createElement('div');
+    const progress = document.createElement('progress');
+    const timerText = document.createElement('p');
+
+    container.className = 'previewCountdownContainer';
+    progress.value = 0;
+    progress.max = 10;
+
+    let timer = 10;
+    let setTimer = setInterval(() =>{
+        if(timer <= 0) {
+            clearInterval(setTimer);
+        };
+
+        progress.value = 10 - timer;
+        timerText.innerHTML = timer + ' sekunder kvar';
+
+        timer -= 1;
+    }, 1000);
+
+    container.append(progress, timerText);
+    app.appendChild(container);
+;}
--- frontend/scripts/information.js
+++ frontend/scripts/information.js
@@ -3,12 +3,12 @@ import { startGame } from "./gameplay";
 const app = document.querySelector('#app');
 
 export function renderStartInformation() {
-    const infoContainer = document.createElement('div');
+    const infoContainer = document.createElement('article');
+    const ruleContainer = document.createElement('div');
     const heading = document.createElement('h2');
     const information = document.createElement('ul');
     const rules = ['First rule', 'second rule', 'third rule'];
     const startBtn = document.createElement('button');
-
 
     rules.forEach(rule => {
         const li = document.createElement('li');
@@ -18,12 +18,14 @@ export function renderStartInformation() {
     });
 
     infoContainer.className = 'infoContainer';
+    ruleContainer.className = 'ruleContainer';
     startBtn.className = 'startBtn';
 
-    heading.innerHTML = 'Regler';
+    heading.innerHTML = 'Regler & Info';
     startBtn.innerHTML = 'Starta';
 
-    infoContainer.append(startBtn, heading, information);
+    ruleContainer.append(heading, information);
+    infoContainer.append(startBtn, ruleContainer);
     app.appendChild(infoContainer);
 
     startBtn.addEventListener('click', startGame);
