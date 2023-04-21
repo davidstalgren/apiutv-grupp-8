d4d04287c339d96019de116e45ba8589f9403e7e
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-04-21 16:00:00 +0200

    add more functionality on admin page

--- frontend/scripts/admin.js
+++ frontend/scripts/admin.js
@@ -8,22 +8,80 @@ export function initAdminMode() {
     app.innerHTML = '';
 
     let colorSelectorDiv = document.createElement('div');
+
     let color0 = document.createElement('button');
+    color0.classList.add('admin__changeColorBtn');
+    color0.innerText = '#F2F2F2';
+    color0.style.backgroundColor = '#F2F2F2';
+
     let color1 = document.createElement('button');
+    color1.classList.add('admin__changeColorBtn');
+    color1.innerText = '#DC2121';
+    color1.style.backgroundColor = '#DC2121';
+
     let color2 = document.createElement('button');
+    color2.classList.add('admin__changeColorBtn');
+    color2.innerText = '#FFDF36';
+    color2.style.backgroundColor = '#FFDF36';
+
     let color3 = document.createElement('button');
+    color3.classList.add('admin__changeColorBtn');
+    color3.innerText = '#3648EC';
+    color3.style.backgroundColor = '#3648EC';
+
     let color4 = document.createElement('button');
+    color4.classList.add('admin__changeColorBtn');
+    color4.innerText = '#43B241';
+    color4.style.backgroundColor = '#43B241';
 
     colorSelectorDiv.append(color0, color1, color2, color3, color4);
 
     app.append(colorSelectorDiv);
-
     
+    color0.addEventListener('click', () => {
+        return userColor = 0;
+    });
+    
+    color1.addEventListener('click', () => {
+        return userColor = 1;
+    });
+    
+    color2.addEventListener('click', () => {
+        return userColor = 2;
+    });
+    
+    color3.addEventListener('click', () => {
+        return userColor = 3;
+    });
+    
+    color4.addEventListener('click', () => {
+        return userColor = 4;
+    });
 
     renderAdminGridContainer();
 }
 
+
+
 const starterGrid = [
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
+]
+
+let gridLayout = [
     [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
     [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
     [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
@@ -81,31 +139,17 @@ export function gridDrawing(gridLayout) {
     }
 }
 
-let gridLayout = [
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-    [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0],        
-]
 
-export function pixelClick(i, j, userColor) {
 
+function pixelClick(i, j, userColor) {
+    
     if (gridLayout[i][j] === userColor) {
         gridLayout[i][j] = 0;
+
         return;
     }
     gridLayout[i][j] = userColor;
 
-    console.log(i, j, userColor);
+    gridDrawing(gridLayout);
+
 }
